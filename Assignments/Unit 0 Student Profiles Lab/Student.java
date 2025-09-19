5e1bde03baf873fedee274e63df1c661e0419d24
mlitt1 <mlitt1@hwemail.com>
2025-09-19 13:28:57 -0700

    worked on student
    Student

--- Assignments/Unit 0 Student Profiles Lab/Student.java
+++ Assignments/Unit 0 Student Profiles Lab/Student.java
@@ -1,46 +1,56 @@
 public class Student {
     // instance variables
     public int grade;
-    public String name, id;
+    public String name;
+    public String id;
     // constructors
     public Student(String name, int grade) {
         this.name = name;
         this.grade = grade;
         this.id = generateId();
     }
+
     // getters
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public int getGrade(){
+    public int getGrade() {
         return grade;
     }
 
-    public String getId(){
-        return generateId();
+    public String getId() {
+        return id;
     }
     
     // setters
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
     
-    public void setId(String id){
+    public void setId(String id) {
         this.id = generateId();
     }
 
-    public void setGrade(int grade){
+    public void setGrade(int grade) {
         this.grade = grade;
     }
 
     // methods
     public String generateId() {
-        return (Math.random() * 9) + (Math.random() * 9) + (Math.random() * 9) + "-" + (Math.random() * 9)+ (Math.random() * 9) + (Math.random() * 9) + (Math.random() * 9);
-    }
+        return ""
+            + (int)(Math.random() * 7 + 1)
+            + (int)(Math.random() * 7 + 1)
+            + (int)(Math.random() * 7 + 1)
+            + "-"
+            + (int)(Math.random() * 10)
+            + (int)(Math.random() * 10)
+            + (int)(Math.random() * 10)
+            + (int)(Math.random() * 10);
+}
 
-    public String toString(int grade, String name, String id){
-        return name + " is a" + grade + "th grade student. Their id is " + id;
-    }
     
+    public String toString() {
+        return name + " is a " + grade + "th grade student. Their id is " + id;
+    }
 
 }
---
+++ Assignments/Unit 0 Student Profiles Lab/StudentTester.java
@@ -0,0 +1,14 @@
+public class StudentTester {
+    
+    public static void main(String[] args) {
+        Student justin = new Student("Justin", 10);
+        System.out.println(justin.toString());
+        Student owen = new Student("Owen", 11);
+        System.out.println(owen.toString());
+        Student morgan = new Student("Morgan", 12);
+        System.out.println(morgan.toString());
+        Student ryan = new Student("Ryan", 11);
+        System.out.println(ryan.toString());
+    }
+
+}
